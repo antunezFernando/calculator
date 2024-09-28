7743f496501709dfac1e6501194b6e3854b2e350
antunezFernando <antunez.fernandoivan.43377@gmail.com>
2024-09-28 20:54:45 -0300

    Add number and operations on screen on button presses

---
+++ script.js
@@ -0,0 +1,119 @@
+let frame = document.querySelector("#frame ");
+
+let operation = document.querySelector("#operation-input");
+let numbers = document.querySelector("#number-input");
+
+let zero = document.querySelector("#zero");
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
+let comma = document.querySelector("#comma");
+
+startListeners()
+
+
+
+function startListeners() {
+    startNumberListeners();
+    startOperationListeners();
+}
+
+function startNumberListeners() {
+    frame.addEventListener("click", (e) => {
+        switch(e.target.id) {
+            case "zero":
+                if(numbers.textContent !== "0") {
+                    numbers.textContent += "0";
+                }
+                break;
+            case "one":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "1";
+                } else {
+                    numbers.textContent += "1";
+                }
+                break;
+            case "two":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "2";
+                } else {
+                    numbers.textContent += "2";
+                }
+                break;
+            case "three":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "3";
+                } else {
+                    numbers.textContent += "3";
+                }
+                break;
+            case "four":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "4";
+                } else {
+                    numbers.textContent += "4";
+                }
+                break;
+            case "five":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "5";
+                } else {
+                    numbers.textContent += "5";
+                }
+                break;
+            case "six":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "6";
+                } else {
+                    numbers.textContent += "6";
+                }
+                break;
+            case "seven":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "7";
+                } else {
+                    numbers.textContent += "7";
+                }
+                break;
+            case "eight":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "8";
+                } else {
+                    numbers.textContent += "8";
+                }
+                break;
+            case "nine":
+                if(numbers.textContent === "0") {
+                    numbers.textContent = "9";
+                } else {
+                    numbers.textContent += "9";
+                }
+                break;
+        }
+    });
+}
+
+function startOperationListeners() {
+    frame.addEventListener("click", (e) => {
+        switch(e.target.id) {
+            case "add":
+                operation.textContent = "+";
+                break;
+            case "subtract":
+                operation.textContent = "-";
+                break;
+            case "multiply":
+                operation.textContent = "×";
+                break;
+            case "divide":
+                operation.textContent = "÷";
+                break;
+        }
+    });
+}
